c4332432bad03fa18ff9265a7da7abf9bd45bec0
Gabriel <gabriel.lins97@gmail.com>
2019-02-24 20:38:57 -0300

    home screen is now ready to receive patients

--- screens/home_screen.py
+++ screens/home_screen.py
@@ -12,7 +12,32 @@ Builder.load_string('''
             size: self.size
             pos: 0, 0
     HomeScreenTopBar:
+        id: top_bar
         pos: 0, root.height - self.height
+    ScreenManager:
+        id: screen_manager
+        size_hint: 1, None
+        height: top_bar.y - dp(6)
+        Screen:
+            id: todos_screen
+            name: 'todos_screen'
+            on_pre_enter:
+                top_bar.ids.todos_btn.style = 'mont-body-selected'
+                top_bar.ids.sem_prognostico_btn.style = 'mont-body'
+            N4Label:
+                text: 'Sem pacientes adicionados.'
+                y: self.parent.height - self.height - dp(10)
+                center_x: self.parent.center_x
+        Screen:
+            id: sem_prognostico_screen
+            name: 'sem_prognostico_screen'
+            on_pre_enter:
+                top_bar.ids.sem_prognostico_btn.style = 'mont-body-selected'
+                top_bar.ids.todos_btn.style = 'mont-body'
+            N4Label:
+                text: 'Não tem na outra tela e você acha que vai ter nessa?'
+                y: self.parent.height - self.height - dp(10)
+                center_x: self.parent.center_x
     N4ImageButton:
         source: './assets/img/add_patient_btn.png'
         pos: root.width - self.width - dp(35), dp(25)
@@ -45,13 +70,25 @@ Builder.load_string('''
         height: dp(33.5)
         halign: 'left'
         padding_x: dp(32)
+        on_press:
+            app.root.change_screen('todos_screen', \
+                direction='right', \
+                transition='slide', \
+                queue_enabled=False, \
+                screen_manager=app.root.ids.home_screen.ids.screen_manager)
     N4Button:
+        id: sem_prognostico_btn
         text: 'Sem Prognóstico'
         style: 'mont-body'
         size_hint_x: 0.5
         height: dp(33.5)
         halign: 'center'
         pos: todos_btn.width, 0
+        on_press:
+            app.root.change_screen('sem_prognostico_screen', \
+                transition='slide', \
+                queue_enabled=False, \
+                screen_manager=app.root.ids.home_screen.ids.screen_manager)
     N4ImageButton:
         x: root.width - self.width
         center_y: title.center_y
